af4cbd30ed73f45f5ea940005befdfe9c93d1777
Nicolas Garnier <ngarnier@mailjet.com>
2018-10-24 23:42:08 +0200

    harmonize refresh method on contact lists

--- src/screens/ContactLists/Lists.js
+++ src/screens/ContactLists/Lists.js
@@ -37,12 +37,36 @@ export default class Lists extends React.Component {
     const { apikeys } = this.props
     const { offset, lists, canLoadMore } = this.state
 
-    if (canLoadMore) {
+    if (method === 'update') {
+      this.setState({
+        offset: 0,
+        isLoading: true,
+      })
+      const updatedLists = await getLists(apikeys.get(0))
+
+      this.setState({
+        lists: updatedLists,
+        isLoading: false,
+        canLoadMore: updatedLists.length === 40,
+      })
+    } else if (method === 'load more' && canLoadMore) {
       const newLists = await getLists(apikeys.get(0), offset + 40)
       this.setState({
         lists: [...lists, ...newLists],
         offset: offset + 40,
         canLoadMore: newLists.length === 40,
+      })
+    } else if (method === 'refresh') {
+      this.setState({
+        isRefreshing: true,
+      })
+
+      const refreshedLists = await getLists(apikeys.get(0))
+
+      this.setState({
+        lists: refreshedLists,
+        isRefreshing: false,
+        canLoadMore: refreshedLists.length === 40,
       })
     }
   }
