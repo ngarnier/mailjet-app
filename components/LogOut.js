a6d3e4a77b4dcedb0317a5705817d99a73cd028c
Nicolas Garnier <ngarnier@mailjet.com>
2018-12-01 12:54:44 +0100

    display alert before log out

--- components/LogOut.js
+++ components/LogOut.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Alert } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Feather } from '@expo/vector-icons'
@@ -8,19 +9,31 @@ import { removeApiKey } from '../actions/apikeys'
   removeApiKeyConnected: removeApiKey,
 })
 
-export default class SettingsGear extends React.Component {
+export default class LogOut extends React.Component {
   static propTypes = {
     removeApiKeyConnected: PropTypes.func.isRequired,
   }
 
-  render() {
+  displayAlert = () => {
     const { removeApiKeyConnected } = this.props
+
+    Alert.alert(
+      'Log out',
+      'Are you sure you want to log out of your Mailjet account?',
+      [
+        { text: 'Cancel', onPress: () => {}, style: 'cancel' },
+        { text: 'OK', onPress: () => removeApiKeyConnected() },
+      ],
+    )
+  }
+
+  render() {
     return (
       <Feather
         name="log-out"
         color="green"
         style={{ fontSize: 28, paddingRight: 10, color: '#1FBE9F' }}
-        onPress={() => removeApiKeyConnected()}
+        onPress={() => this.displayAlert()}
       />
     )
   }
